e5a713e32f6da3f6fef426a878a1776c116260d9
NathanBuildsDIY <165212390+NathanBuildsDIY@users.noreply.github.com>
2024-03-28 12:21:53 -0500

    Update initial-setup.sh
    swap print for echo

--- initial-setup.sh
+++ initial-setup.sh
@@ -1,8 +1,8 @@
-print("get up to date packages with apt-get")
+echo "get up to date packages with apt-get"
 sudo apt-get update
 sudo apt-get -y upgrade
 
-print("install new packages - various stuff to run weeder script")
+echo "install new packages - various stuff to run weeder script"
 sudo apt-get -y install pip
 sudo apt-get -y install python3-pyqt5 #only on 64 0 2, camera failed
 sudo apt-get -y install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev wget
@@ -10,19 +10,19 @@ sudo apt-get -y install libreadline-gplv2-dev libncursesw5-dev libssl-dev libsql
 sudo apt-get -y remove vim-tiny
 sudo apt-get -y install vim
 
-print("use pip to install packages into python")
+echo "use pip to install packages into python"
 pip3 install opencv-python
 pip3 install --upgrade tflite-support==0.4.2
 pip3 install --upgrade tflite-runtime==2.11.0
 pip3 install imutils
 
-print("get pigpio daemon running so we have better servo control")
+echo "get pigpio daemon running so we have better servo control"
 sudo apt-get -y install pigpio python-pigpio python3-pigpio
 sudo apt-get -y install pigpiod
 pip3 install pigpio
 sudo systemctl enable pigpiod #makes it run at startup (after reboot - sudo shutdown -r now. Check with pigs t)
 
-print("set up iptables and rules")
+echo "set up iptables and rules"
 sudo apt-get -y install iptables
 sudo iptables -t nat -I PREROUTING -p tcp --dport 443 -j REDIRECT --to-ports 5000
 sudo iptables -t nat -I OUTPUT -p tcp -o lo --dport 443 -j REDIRECT --to-ports 5000
@@ -30,7 +30,7 @@ sudo iptables -t nat -I PREROUTING -p tcp --dport 80 -j REDIRECT --to-ports 5000
 sudo iptables -t nat -I OUTPUT -p tcp -o lo --dport 80 -j REDIRECT --to-ports 5000
 sudo iptables-save
 
-print("set up dnsmask to do dns resolution on our wifi hotspot")
+echo "set up dnsmask to do dns resolution on our wifi hotspot"
 sudo apt-get -y install dnsmasq
 sudo apt-get -y install dnsutils
 #NOTE: I thought dnsmask woudl work like this:
@@ -38,10 +38,11 @@ sudo apt-get -y install dnsutils
 #but once wifi hotspot is actually set up we instead config in this file below
 echo 'address=/.local/10.42.0.1' | sudo tee -a /etc/NetworkManager/dnsmasq-shared.d/hosts.conf
 
-#TO DO: add to crontab - python3 -m flask --app ~/weeder/webgui.py run --host=0.0.0.0
-print("create entry in crontab to always run weeder app on startup")
+echo "create entry in crontab to always run weeder app on startup"
+line="@reboot python3 -m flask --app ~/weeder/WeedKiller_v6.py run --host=0.0.0.0"
+(crontab -u $(whoami) -l; echo "$line" ) | crontab -u $(whoami) -
 
-print("Install flask and associated forms packages")
+echo "Install flask and associated forms packages"
 pip3 install -U Flask 
 pip3 install WTForms
 pip3 install Flask-WTF
