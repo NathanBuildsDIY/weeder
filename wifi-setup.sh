5a0e6a1547a7bd6907aa5a9182f7c4ca90a24528
NathanBuildsDIY <165212390+NathanBuildsDIY@users.noreply.github.com>
2024-03-28 12:22:29 -0500

    Update wifi-setup.sh
    swap echo for print

--- wifi-setup.sh
+++ wifi-setup.sh
@@ -1,4 +1,4 @@
-print("setup local wifi hotspot")
+echo "setup local wifi hotspot"
 sudo nmcli device wifi hotspot ssid weeder password LetsWeed ifname wlan0
   #note - to disable, sudo nmcli device disconnect wlan0
   #After disabling the network, run the following command to reconnect to another Wi-Fi network: sudo nmcli device up wlan0
@@ -7,7 +7,7 @@ UUID=$(nmcli connection | grep Hotspot | tr -s ' ' | cut -d ' ' -f 2)
 #nmcli connection show $UUID
 sudo nmcli connection modify $UUID connection.autoconnect yes connection.autoconnect-priority 100
 
-print("setup complete - check over output, then doing a reboot")
+echo "setup complete - check over output, then doing a reboot"
 sleep 200
 sudo shutdown -r now
 
