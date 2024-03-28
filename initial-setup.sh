94c333c2e93a0b4936388fa822d4cc2f16f502c3
NathanBuildsDIY <165212390+NathanBuildsDIY@users.noreply.github.com>
2024-03-28 14:39:27 -0500

    Update initial-setup.sh
    add network setup here

--- initial-setup.sh
+++ initial-setup.sh
@@ -50,5 +50,18 @@ pip3 install Flask-WTF
 pip3 install flask_autoindex
 pip3 install psutil
 
+echo "setup local wifi hotspot"
+sudo systemctl stop dhcpcd
+sudo systemctl disable dhcpcd
+sudo systemctl enable NetworkManager 
+sudo service NetworkManager start
+sudo nmcli device wifi hotspot ssid weeder password LetsWeed ifname wlan0
+  #note - to disable, sudo nmcli device disconnect wlan0
+  #After disabling the network, run the following command to reconnect to another Wi-Fi network: sudo nmcli device up wlan0
+#once enabled, we need to make it priority so it always runs on startup
+UUID=$(nmcli connection | grep Hotspot | tr -s ' ' | cut -d ' ' -f 2)
+#nmcli connection show $UUID
+sudo nmcli connection modify $UUID connection.autoconnect yes connection.autoconnect-priority 100
+
 echo "restarting for changes to take effect. Still must run wifi setup"
 sudo shutdown -r now
